644a3cb970e3dddba49ace9d947691a0aaacdfc4
kelvinnguyen3196 <kelvin.nguyen.360@gmail.com>
2021-11-24 17:21:45 -0800

    Created canvas and added some features
    Added a tool preview that responds to the size slider

--- public/js/canvas.js
+++ public/js/canvas.js
@@ -1,3 +1,40 @@
-document.getElementById('brush-size').onchange = () => {
-    console.log(document.getElementById('brush-size').value);
+let s = sketch => {
+    let height = document.getElementById('canvas-container').offsetHeight;
+    let width = document.getElementById('canvas-container').offsetWidth;
+
+    let toolPreviewLayer;
+
+    let toolRadius = document.getElementById('brush-size').value;
+
+    sketch.setup = () => {
+        sketch.createCanvas(width, height);
+
+        toolPreviewLayer = sketch.createGraphics(width, height);
+        toolPreviewLayer.clear();
+    }
+
+    sketch.draw = () => {
+        sketch.background('lightblue');
+
+        toolPreviewLayer.clear();
+        toolPreviewLayer.noFill();
+        toolPreviewLayer.stroke(0);
+        toolPreviewLayer.ellipse(sketch.mouseX, sketch.mouseY, toolRadius, toolRadius);
+        // tool preview should always be last line of code executed to be on top
+        sketch.image(toolPreviewLayer, 0, 0, width, height);
+    }
+
+    document.getElementById('brush-size').oninput = () => {
+        toolRadius = document.getElementById('brush-size').value;
+    }
 }
+
+let p5v1 = new p5(s, 'canvas-container');
+// function setup() {
+//     let c = createCanvas(400, 400);
+//     c.parent('canvas-container');
+// }
+
+// function draw() {
+//     background('blue');
+// }
